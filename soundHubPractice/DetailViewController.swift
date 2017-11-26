82527118eefe54432a6444778b88c45c1574c462
e-sung <dev.esung@gmail.com>
2017-11-26 19:46:42 +0900

    Add DetailHeaderCell

--- soundHubPractice/DetailViewController.swift
+++ soundHubPractice/DetailViewController.swift
@@ -8,10 +8,26 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "detailHeaderCell", for: indexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
 
+    @IBOutlet weak var detailTV: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        detailTV.delegate = self
+        detailTV.dataSource = self
 
         // Do any additional setup after loading the view.
     }
