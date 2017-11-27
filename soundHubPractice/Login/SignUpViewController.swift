392911fe244b0e4beb68f6bd2d1e4b3523faf087
e-sung <dev.esung@gmail.com>
2017-11-27 20:48:52 +0900

    Add SignUpVC

---
+++ soundHubPractice/Login/SignUpViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SignUpViewController.swift
+//  soundHubPractice
+//
+//  Created by 류성두 on 2017. 11. 27..
+//  Copyright © 2017년 류성두. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
