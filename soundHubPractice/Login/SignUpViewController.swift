0a1b160096bf92bee621176edb5ea9c1aed50762
e-sung <dev.esung@gmail.com>
2017-11-27 21:09:24 +0900

    Finish SignUpVC

--- soundHubPractice/Login/SignUpViewController.swift
+++ soundHubPractice/Login/SignUpViewController.swift
@@ -8,16 +8,44 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
+    var isKeyboardUp = false
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var nickNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var passwordConfirmTF: UITextField!
+    
+    @IBAction func emailConfirmHandler(_ sender: UITextField) {
+        sender.resignFirstResponder()
+        nickNameTF.becomeFirstResponder()
+    }
+    @IBAction func nickNameConfirmHandler(_ sender: UITextField) {
+        sender.resignFirstResponder()
+        passwordTF.becomeFirstResponder()
+    }
+    @IBAction func passwordConfirmHandler(_ sender: UITextField) {
+        sender.resignFirstResponder()
+        passwordConfirmTF.becomeFirstResponder()
+    }
+    @IBAction func passwordFinalConfirmHandler(_ sender: UITextField) {
+        print("asdf")
+    }
+    @IBAction func onTouchHandler(_ sender: UITapGestureRecognizer) {
+        if isKeyboardUp {
+            self.view.endEditing(true)
+        }else{
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardDidShow, object: nil, queue: nil) { (noti) in
+            self.isKeyboardUp = true
+        }
+        NotificationCenter.default.addObserver(forName: NSNotification.Name.UIKeyboardDidHide, object: nil, queue: nil) { (noti) in
+            self.isKeyboardUp = false
+        }
     }
 }
