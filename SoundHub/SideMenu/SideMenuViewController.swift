423ba532a134d0d2df3444f69bfea660d3b2c0ac
e-sung <dev.esung@gmail.com>
2017-11-29 12:26:30 +0900

    Implement Logout Function More Properly

---
+++ SoundHub/SideMenu/SideMenuViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SideMenuViewController.swift
+//  SoundHub
+//
+//  Created by 류성두 on 2017. 11. 29..
+//  Copyright © 2017년 류성두. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+
+    @IBAction func logoutButtonHandler(_ sender: UIButton) {
+        self.view.window!.rootViewController?.dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
