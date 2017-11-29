1d923a3f2458c3189a606dbded88560ba2c814b7
e-sung <dev.esung@gmail.com>
2017-11-29 12:59:23 +0900

    Add ProfileSetUp VC

---
+++ SoundHub/SideMenu/ProfileHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileHeaderCell.swift
+//  SoundHub
+//
+//  Created by 류성두 on 2017. 11. 29..
+//  Copyright © 2017년 류성두. All rights reserved.
+//
+
+import UIKit
+
+class ProfileHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ SoundHub/SideMenu/ProfileViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ProfileViewController.swift
+//  SoundHub
+//
+//  Created by 류성두 on 2017. 11. 29..
+//  Copyright © 2017년 류성두. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBAction func confirmButtonHandler(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "profileHeaderCell", for: indexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 330
+    }
+
+    
+    @IBOutlet weak var mainTV: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainTV.delegate = self
+        mainTV.dataSource = self
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
