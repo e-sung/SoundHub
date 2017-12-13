7b4105c7b7db9313b115531f6965668402e3b835
e-sung <dev.esung@gmail.com>
2017-12-13 19:37:57 +0900

    WaveForm Render Faster WIth Smaller BufferSize

--- SoundHub/Activities/DetailView/CustomCell/MasterWaveFormViewCell.swift
+++ SoundHub/Activities/DetailView/CustomCell/MasterWaveFormViewCell.swift
@@ -7,29 +7,50 @@
 //
 
 import UIKit
+import AudioKit
+import AudioKitUI
 import FDWaveformView
 import NVActivityIndicatorView
 
 class MasterWaveFormViewCell: UITableViewCell, FDWaveformViewDelegate {
 
+//    @IBOutlet weak var audioPlot: EZAudioPlot!
+    
     @IBOutlet weak private var activityIndicator: NVActivityIndicatorView!
-    @IBOutlet weak private var waveForm: FDWaveformView!
+ 
     var masterAudioURL:URL?{
         willSet(newVal){
-            waveForm.audioURL = newVal
+            let audioPlot = EZAudioPlot(frame:self.contentView.frame)
+            audioPlot.shouldFill = true
+            audioPlot.plotType = .buffer
+            audioPlot.shouldMirror = true
+            audioPlot.color = .orange
+            self.addSubview(audioPlot)
+            DispatchQueue.global().async {
+                let file = EZAudioFile(url: newVal!)!
+                let bufferLength:UInt32 = 512
+                let waveData = file.getWaveformData(withNumberOfPoints: bufferLength)
+                let buffer = waveData?.buffers[0]
+                DispatchQueue.main.async {
+                    audioPlot.updateBuffer(buffer, withBufferSize: bufferLength)
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+
+//            waveForm.audioURL = newVal
         }
     }
 
-    func waveformViewDidRender(_ waveformView: FDWaveformView) {
-        activityIndicator.stopAnimating()
-    }
+//    func waveformViewDidRender(_ waveformView: FDWaveformView) {
+//        activityIndicator.stopAnimating()
+//    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
 
-        waveForm.delegate = self
-        waveForm.wavesColor = .orange
-        waveForm.loadingInProgress = true
+//        waveForm.delegate = self
+//        waveForm.wavesColor = .orange
+//        waveForm.loadingInProgress = true
         activityIndicator.color = .orange
         activityIndicator.type = .audioEqualizer
         activityIndicator.startAnimating()
