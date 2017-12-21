273c5c0d03c47c1830adc2019a1318898efa5fc1
e-sung <dev.esung@gmail.com>
2017-12-21 23:48:17 +0900

    Finish

--- SoundHub/EntryPoint/AudioUploadViewController.swift
+++ SoundHub/EntryPoint/AudioUploadViewController.swift
@@ -17,6 +17,7 @@ class AudioUploadViewController: UIViewController {
     var audioURL:URL?
     var genre:String!
     var instrument:String!
+    private let imagePicker = UIImagePickerController()
     @IBOutlet weak private var audioTitleTF:UITextField!
     @IBOutlet weak private var albumArt: UIButton!
     @IBOutlet weak private var authorNameLB: UILabel!
@@ -24,6 +25,9 @@ class AudioUploadViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
     }
     
+    @IBAction func onAlbumArtClickHandler(_ sender: UIButton) {
+        present(photoSourceChooingAlert, animated: true, completion: nil)
+    }
     @IBAction func onViewTouchHandler(_ sender: UITapGestureRecognizer) {
         self.view.endEditing(true)
     }
@@ -43,7 +47,10 @@ class AudioUploadViewController: UIViewController {
             let artistMetadata = String.generateAvMetaData(with: authorNameLB.text!,
                                                            and: .commonIdentifierArtist)
             let exportURL = URL(string: "\(audioTitle).m4a".addingPercentEncoding(withAllowedCharacters: CharacterSet.urlPathAllowed)! , relativeTo: DataCenter.documentsDirectoryURL)!
-            LPSnackbar.showSnack(title: "Exporting...")
+            self.dismissWith(depth: 2, from: self, completion: {
+                LPSnackbar.showSnack(title: "Exporting...")
+            })
+            
 //            RecordConductor.main.exportRecordedAudio(to: exportURL,
 //                                                     with: [titleMetadata, artistMetadata], completion: {
 //                NetworkController.main.uploadAudio(In: exportURL, genre: genre, instrument: instrument, completion: {
@@ -73,6 +80,8 @@ class AudioUploadViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imagePicker.allowsEditing = false
+        imagePicker.delegate = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -82,5 +91,46 @@ class AudioUploadViewController: UIViewController {
         guard let audioURL = audioURL else { return }
         let playerItem = AVPlayerItem(url: audioURL)
         setUpUI(with: playerItem)
+    }
+    
+    private var defaultUIAlertActions:[UIAlertAction]{
+        get{
+            let withExistingPhoto = UIAlertAction(title: "원래 있던 사진으로", style: .default , handler: { (action) in
+                self.imagePicker.sourceType = .photoLibrary
+                self.present(self.imagePicker, animated: true, completion: nil)
+            })
+            
+            let withNewPhoto = UIAlertAction(title: "새로 사진 찍어서", style: .default , handler: { (action) in
+                self.imagePicker.sourceType = .camera
+                self.present(self.imagePicker, animated: true, completion: nil)
+            })
+            
+            let cancel = UIAlertAction(title: "취소", style: .cancel) { (action) in
+                self.presentedViewController?.dismiss(animated: true, completion: nil)
+            }
+            return [withExistingPhoto, withNewPhoto, cancel]
+        }
+    }
+    
+    private var photoSourceChooingAlert:UIAlertController{
+        get{
+            let alert = UIAlertController(title: "사진 변경", message: "", preferredStyle: .actionSheet)
+            let actions = defaultUIAlertActions
+            for action in actions{
+                alert.addAction(action)
+            }
+            return alert
+        }
+    }
+}
+
+// MARK: ImagePickerDelegate
+extension AudioUploadViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            albumArt.setImage(pickedImage, for: .normal)
+            albumArt.imageView?.contentMode = .scaleAspectFill
+        }
+        dismiss(animated: true, completion: nil)
     }
 }
